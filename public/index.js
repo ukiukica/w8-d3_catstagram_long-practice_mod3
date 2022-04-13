7fc55b69f4b217b71a9192a85d9d55520e252d2d
uki <ukiworks034@gmail.com>
2022-04-13 17:23:36 -0400

    buttons added

--- public/index.js
+++ public/index.js
@@ -14,9 +14,35 @@ const initializePage = () => {
 window.onload = () => {
     initializePage();
     createTitle();
+
+    let count = 0;
+
     const buttonNew = document.createElement("button");
     buttonNew.innerText = "Get Cat";
     document.body.append(buttonNew);
     buttonNew.addEventListener("click", createMainContent)
 
+    const score = document.createElement('h4');
+    document.body.append(score)
+    score.innerText = `Popularity Score: ${count}`
+
+    const buttonUp = document.createElement("button");
+    buttonUp.innerText = "Upvote";
+    document.body.append(buttonUp);
+    buttonUp.addEventListener("click", upvote)
+
+    const buttonDown = document.createElement("button");
+    buttonDown.innerText = "Downvote";
+    document.body.append(buttonDown);
+    buttonDown.addEventListener("click", downvote)
+
+    function upvote() {
+        count++;
+        score.innerText = `Popularity Score: ${count}`
+    }
+
+    function downvote() {
+        count--;
+        score.innerText = `Popularity Score: ${count}`
+    }
 };
